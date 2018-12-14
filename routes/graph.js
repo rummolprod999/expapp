8f1412a86c66f4d76e9e85ddc24bbc5c92584a34
rummolprod999 <rummolprod999@gmail.com>
2018-12-14 10:40:17 +0300

    fix issues

--- routes/graph.js
+++ routes/graph.js
@@ -3,9 +3,9 @@ let router = express.Router();
 let fun = require('../fun_tools');
 router.get('/:tenderType', function(req, res, next) {
     let tenType = req.params['tenderType'];
-    res.render('graph', { title: `График полученных и обновленных тендеров ${tenType}`, tenderT: tenType});
+    res.render('graph', { title: `График полученных и обновленных документов ${tenType}`, tenderT: tenType});
 });
 router.get('/', function(req, res, next) {
-    res.render('graphall', { title: `Все графики полученных и обновленных тендеров`});
+    res.render('graphall', { title: `Все графики полученных и обновленных документов`});
 });
 module.exports = router;
