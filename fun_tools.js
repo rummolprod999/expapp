39db2de9b3eb379da22c2c21851e7179484aa5c5
rummolprod999 <rummolprod999@gmail.com>
2019-08-07 13:28:22 +0300

    add new places

--- fun_tools.js
+++ fun_tools.js
@@ -650,7 +650,8 @@ map.set('Tenders44Fz', 'ParserTenders/log_tenders44')
     .set('TendersTj', 'ParserWebFSharp/log_tenders_tj')
     .set('TendersPromUa', 'ParserPromUa/logdir_PromUa')
     .set('TendersUzex', 'ParserKotlinNew/logdir_tenders_uzex')
-    .set('TendersTurk', 'ParserWebFSharp/log_tenders_turk');
+    .set('TendersTurk', 'ParserWebFSharp/log_tenders_turk')
+    .set('TendersAchi', 'ParserKotlinNew/logdir_tenders_achi');
 
 let map_description = new Map().set('Tenders44Fz', 'Закупки с http://zakupki.gov.ru/, ФЗ 44')
     .set('Tenders223Fz', 'Закупки с http://zakupki.gov.ru/, ФЗ 223')
@@ -872,7 +873,8 @@ let map_description = new Map().set('Tenders44Fz', 'Закупки с http://zak
     .set('TendersTj', 'Закупки с http://test.zakupki.gov.tj/, все')
     .set('TendersPromUa', 'Закупки с https://zakupki.prom.ua/, все')
     .set('TendersUzex', 'Закупки с https://dxarid.uzex.uz/, все')
-    .set('TendersTurk', 'Закупки с https://turkmenportal.com/, все');
+    .set('TendersTurk', 'Закупки с https://turkmenportal.com/, все')
+    .set('TendersAchi', 'Закупки с https://achizitii.md/, все');
 
 let export_map = [];
 for (let m of map) {
