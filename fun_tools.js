a9ec2489c547f232b2871588ce3588c47a609d75
rummolprod999 <rummolprod999@gmail.com>
2019-11-26 14:14:48 +0300

    add new places

--- fun_tools.js
+++ fun_tools.js
@@ -663,7 +663,8 @@ map.set('Tenders44Fz', 'ParserTenders/log_tenders44')
     .set('TendersSportMaster', 'ParserWebCore/log_sportmaster')
     .set('TendersCisLink', 'ParserWebFSharp/log_tenders_cislink')
     .set('TendersTekSil', 'ParserWebCore/log_teksil')
-    .set('TendersPetr', 'ParserWebFSharp/log_tenders_petr');
+    .set('TendersPetr', 'ParserWebFSharp/log_tenders_petr')
+    .set('TendersSberb2b', 'ParserWebCore/log_sberb2b');
 
 let map_description = new Map().set('Tenders44Fz', 'Закупки с http://zakupki.gov.ru/, ФЗ 44')
     .set('Tenders223Fz', 'Закупки с http://zakupki.gov.ru/, ФЗ 223')
@@ -898,7 +899,8 @@ let map_description = new Map().set('Tenders44Fz', 'Закупки с http://zak
     .set('TendersSportMaster', 'Закупки с http://zakupki.sportmaster.ru/, все')
     .set('TendersCisLink', 'Закупки с http://auction.cislink.com/, все')
     .set('TendersTekSil', 'Закупки с https://www.tektorg.ru/silovyi_machine/procedures/, все')
-    .set('TendersPetr', 'Закупки с https://eshop-ptz.ru/purchases/, все');
+    .set('TendersPetr', 'Закупки с https://eshop-ptz.ru/purchases/, все')
+    .set('TendersSberb2b', 'Закупки с https://sberb2b.ru/, все');
 
 let export_map = [];
 for (let m of map) {
