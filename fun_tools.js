1f4c6f7c7a9cc2f8b8c9c7f4be6bed5d3d6bb212
rummolprod999 <rummolprod999@gmail.com>
2020-02-06 09:07:49 +0300

    add new places

--- fun_tools.js
+++ fun_tools.js
@@ -680,7 +680,8 @@ map.set('Tenders44Fz', 'ParserTenders/log_tenders44')
     .set('TendersVolgZmo', 'ParserWebFSharp/log_tenders_volgzmo')
     .set('TendersZakupMos', 'ParserWebCore/log_zakupmos')
     .set('TendersRusal', 'ParserWebFSharp/log_tenders_rusal')
-    .set('TendersNordstar', 'WebParserRust/logdir_nordstar');
+    .set('TendersNordstar', 'WebParserRust/logdir_nordstar')
+    .set('TendersProtek', 'ParserKotlinNew/logdir_tenders_protek');
 
 let map_description = new Map().set('Tenders44Fz', 'Закупки с http://zakupki.gov.ru/, ФЗ 44')
     .set('Tenders223Fz', 'Закупки с http://zakupki.gov.ru/, ФЗ 223')
@@ -932,7 +933,8 @@ let map_description = new Map().set('Tenders44Fz', 'Закупки с http://zak
     .set('TendersVolgZmo', 'Закупки с https://szvo.gov35.ru/, все')
     .set('TendersZakupMos', 'Закупки с https://zakupki.mos.ru/, все')
     .set('TendersRusal', 'Закупки с https://rusal.ru/, все')
-    .set('TendersNordstar', 'Закупки с https://nordstar.ru/, все');
+    .set('TendersNordstar', 'Закупки с https://nordstar.ru/, все')
+    .set('TendersProtek', 'Закупки с https://protek.ru/, все');
 
 let export_map = [];
 for (let m of map) {
