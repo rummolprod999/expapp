9720f1bb4d1b432c3f7de2f61a550f1f3bf8e6e1
rummolprod999 <rummolprod999@gmail.com>
2020-11-19 18:36:30 +0300

    add new places

--- fun_tools.js
+++ fun_tools.js
@@ -727,7 +727,8 @@ map.set('Tenders44Fz', 'ParserTenders/log_tenders44')
     .set('TendersGoldenSeed', 'ParserWebFSharp/log_tenders_goldenseed')
     .set('TendersKaustik', 'ParserWebFSharp/log_tenders_kaustik')
     .set('TendersDme', 'ParserWebFSharp/log_tenders_dme')
-    .set('TendersTele2', 'ParserWebFSharp/log_tenders_tele2');
+    .set('TendersTele2', 'ParserWebFSharp/log_tenders_tele2')
+    .set('TendersOsnova', 'ParserWebFSharp/log_tenders_osnova');
 
 let map_description = new Map().set('Tenders44Fz', 'Закупки с http://zakupki.gov.ru/, ФЗ 44')
     .set('Tenders223Fz', 'Закупки с http://zakupki.gov.ru/, ФЗ 223')
@@ -1026,7 +1027,8 @@ let map_description = new Map().set('Tenders44Fz', 'Закупки с http://zak
     .set('TendersGoldenSeed', 'Закупки с https://www.goldenseed.ru/, все')
     .set('TendersKaustik', 'Закупки с https://www.kaustik.ru/, все')
     .set('TendersDme', 'Закупки с https://market.dme.aero/, все')
-    .set('TendersTele2', 'Закупки с https://msk.tele2.ru/, все');
+    .set('TendersTele2', 'Закупки с https://msk.tele2.ru/, все')
+    .set('TendersOsnova', 'Закупки с https://tender.gk-osnova.ru/, все');
 
 let export_map = [];
 for (let m of map) {
